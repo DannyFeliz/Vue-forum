709efddaf02bb70a1ec542d0faccf69dbb7fb566
Danny <dannyfeliz08@gmail.com>
2016-08-09 10:50:29 -0400

    Add env.example.js

---
+++ config/env.example.js
@@ -0,0 +1,9 @@
+// Before anything, rename this file to "env.js"
+
+const ENV = {
+  LOG_REQUEST : false,
+  LOG_RESPONSE : false,
+  API_URL: "http://api.example.com", // Replace this with you API URL
+}
+
+export default ENV;
